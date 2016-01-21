46d7822b7fa4fda18c59b53df8b99539fc9680d4
Dmitry Spodarets <d.spodarets@flyelephant.net>
2016-01-22 00:10:11 +0200

    Update Go_MakeTrain.m

--- Go_MakeTrain.m
+++ Go_MakeTrain.m
@@ -121,15 +121,15 @@ parfor NN_No = 1:1
     %save(sprintf('%s\\data_WS', out_path), 'C', 'esr_test', 'max_k');
     %save(sprintf('%s\\_accuracy_%1.2f', out_path, esr_test), 'esr_test');
     
-    if (exist('out.txt'))
-      out = dlmread('out.txt');
+    if (exist('myfileres.txt'))
+      out = dlmread('myfileres.txt');
     else
       out = [];
     end
     
     out = [out;[nlength, NN_No, esr_test]];
     
-    dlmwrite('out.txt', out);
+    dlmwrite('myfileres.txt', out);
 endparfor
 
 % close all;
